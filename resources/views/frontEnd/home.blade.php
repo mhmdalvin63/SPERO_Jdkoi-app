0ded2ef322f2ff5bad0409d64442ff15febeb79c
muhammadalvin92 <alvinbomber87@gmail.com>
2023-06-27 14:16:11 +0700

    Part & Main Content

---
+++ resources/views/frontEnd/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.main')
+@section('judul_tab','Home JDKoi')
+
+@section('content')
+
+<h1>woiiiiiiiiiiii</h1>
+    
+@endsection
---
+++ resources/views/layout/part/navbar.blade.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" href=" {{ asset('../css/part/navbar.css')}}">
+
+<div class="parent">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-4">
+        <div class="container">
+            <a href="#" class="navbar-brand">Brand</a>
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <div class="navbar-nav gap-0 gap-lg-4 ms-auto">
+                  	<a href="#" class="nav-item nav-link active">Home</a>
+                    <a href="#" class="nav-item nav-link">Auction</a>
+                    <a href="#" class="nav-item nav-link">Favorite</a>
+                    <a href="#" class="nav-item nav-link">Breeder</a>
+                    <a href="#" class="nav-item nav-link">Auction Chart</a>
+                    <a href="#" class="nav-item nav-link">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</div>
