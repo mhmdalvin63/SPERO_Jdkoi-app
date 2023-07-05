315bd30b030ecba50d54e46b6fb664d1ce9a2a57
muhammadalvin92 <alvinbomber87@gmail.com>
2023-07-05 14:39:18 +0700

    Update Routing

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () { return view('frontEnd.home'); });
+Route::get('auction', function () { return view('frontEnd.auction'); });
+Route::get('favorite', function () { return view('frontEnd.favorite'); });
+Route::get('breeder', function () { return view('frontEnd.breeder'); });
+Route::get('detailbidder', function () { return view('frontEnd.detailBidder'); });
+Route::get('chart', function () { return view('frontEnd.auctionChart'); });
+Route::get('profile', function () { return view('frontEnd.profile'); });
+
+Route::get('register', function () { return view('frontEnd.register'); });
+Route::get('login', function () { return view('frontEnd.login'); });
+Route::get('lupaPassword', function () { return view('frontEnd.lupaPassword'); });
